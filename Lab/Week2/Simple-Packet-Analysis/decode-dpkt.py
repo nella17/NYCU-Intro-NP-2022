c60b0d527cb2eb6884c51878589667f246c394b1
Allen Guan <me@nella17.tw>
2022-09-27 02:25:24 +0800

    [Lab/Week2] fix dpkt SLL2

--- Lab/Week2/Simple-Packet-Analysis/decode-dpkt.py
+++ Lab/Week2/Simple-Packet-Analysis/decode-dpkt.py
@@ -2,25 +2,29 @@ import dpkt
 from base64 import b64decode
 from sys import argv
 
+def packet2ip(packet):
+    parser = [
+        dpkt.ethernet.Ethernet,
+        dpkt.sll2.SLL2,
+    ]
+    for func in parser:
+        pack = func(packet)
+        if isinstance(pack.data, dpkt.ip.IP):
+            return pack.data
+
 with open(argv[1],'rb') as f:
     pcap = list(dpkt.pcap.UniversalReader(f))
+    pcap = [packet2ip(pkt) for ts,pkt in pcap]
+    pcap = [x for x in pcap if x]
 
 mp = {}
-for ts,pkt in pcap:
-    eth = dpkt.ethernet.Ethernet(pkt)
-    if not isinstance(eth.data, dpkt.ip.IP):
-        continue
-    ip = eth.data
+for ip in pcap:
     k = (ip.tos,ip.ttl)
     if k not in mp:
         mp[k] = 0
     mp[k] += 1
 
-for ts,pkt in pcap:
-    eth = dpkt.ethernet.Ethernet(pkt)
-    if not isinstance(eth.data, dpkt.ip.IP):
-        continue
-    ip = eth.data
+for ip in pcap:
     k = (ip.tos,ip.ttl)
     if mp[k] == 1 and ip.data.data:
         text = b64decode(ip.data.data).decode()
