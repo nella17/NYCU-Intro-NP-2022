88ff9c2403d252ae98798e1bccbf9895f1961b34
Allen Guan <me@nella17.tw>
2022-12-03 13:40:14 +0800

    [Lab/8] faster

--- Lab/8/src/util.hpp
+++ Lab/8/src/util.hpp
@@ -75,7 +75,7 @@ inline void dump_hdr(const struct response_hdr_t* hdr) {
 inline void set_sockopt(int sockfd) {
     struct timeval tv;
     tv.tv_sec = 0;
-    tv.tv_usec = 4 * 1000;
+    tv.tv_usec = 16 * 1000;
     if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0)
         fail("setsockopt(SO_RCVTIMEO)");
     size_t size = 1024 * 1024 * TOTAL_SIZE;
