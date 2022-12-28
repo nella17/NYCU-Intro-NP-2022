44aef003248941f33f966055b639d49fdfe1add6
Allen Guan <me@nella17.tw>
2022-12-29 02:08:50 +0800

    [HW/2] update record.sh

--- Homework/2/record.sh
+++ Homework/2/record.sh
@@ -2,6 +2,7 @@
 set -x
 sudo tcpdump -q -s 0 -n -i lo -X -w dump.pcap -A port 53 &
 reset
+sleep 0.5
 echo
 dig +short google.com
 dig +short google.com A
@@ -11,6 +12,8 @@ dig +short google.com CNAME
 dig +short google.com SOA
 dig +short google.com MX
 dig +short google.com TXT
+dig +short x.google.com
 dig +short q.proto.ctf.nella17.tw
 dig +short q.proto.ctf.nella17.tw CNAME
-kill -SIGQUIT %
+jobs
+kill -SIGTERM %
